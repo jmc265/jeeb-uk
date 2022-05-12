1552b24b977222c18b00b63a545b477b290ae089
James Cross <james@breakingwave.com>
2022-05-12 16:34:08 +0100

    Adding email autodiscover DNS entries

--- .cloud/dns.tf
+++ .cloud/dns.tf
@@ -57,6 +57,44 @@ resource "azurerm_dns_txt_record" "jeeb-uk-email" {
   }
 }
 
+resource "azurerm_dns_cname_record" "jeeb-uk-mail" {
+  name                = "mail"
+  zone_name           = azurerm_dns_zone.jeeb-uk.name
+  resource_group_name = azurerm_resource_group.jeeb-uk.name
+  ttl                 = 300
+  record              = "privateemail.com"
+}
+
+resource "azurerm_dns_cname_record" "jeeb-uk-autodiscover" {
+  name                = "autodiscover"
+  zone_name           = azurerm_dns_zone.jeeb-uk.name
+  resource_group_name = azurerm_resource_group.jeeb-uk.name
+  ttl                 = 300
+  record              = "privateemail.com"
+}
+
+resource "azurerm_dns_cname_record" "jeeb-uk-autoconfig" {
+  name                = "autoconfig"
+  zone_name           = azurerm_dns_zone.jeeb-uk.name
+  resource_group_name = azurerm_resource_group.jeeb-uk.name
+  ttl                 = 300
+  record              = "privateemail.com"
+}
+
+resource "azurerm_dns_srv_record" {
+  name                = "_autodiscover"
+  zone_name           = azurerm_dns_zone.jeeb-uk.name
+  resource_group_name = azurerm_resource_group.jeeb-uk.name
+  ttl                 = 300
+
+  record {
+    priority = 0
+    weight   = 0
+    port     = 443
+    target   = "privateemail.com"
+  }
+}
+
 output "name_servers" {
   value = azurerm_dns_zone.jeeb-uk.name_servers
 }
