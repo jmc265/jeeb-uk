58be85fbc57923887e2c75f239c4f0b3fe53db10
James Cross <james@breakingwave.com>
2022-05-12 11:10:45 +0100

    Apply filter to heading anchor images

--- .eleventy.js
+++ .eleventy.js
@@ -92,7 +92,7 @@ module.exports = function (eleventyConfig) {
     })
         .use(require('markdown-it-modify-token'))
         .use(markdownItGithubHeadings, {
-            className: "heading-anchor",
+            className: "heading-anchor apply-svg-filter",
             prefixHeadingIds: false
         });
     eleventyConfig.setLibrary("md", markdownLibrary);
