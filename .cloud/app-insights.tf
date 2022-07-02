cce76baa8086178f0d116eaca0578016745f1e13
james <jamescross265@gmail.com>
2022-07-02 20:59:37 +0100

    Using workspace

--- .cloud/app-insights.tf
+++ .cloud/app-insights.tf
@@ -1,3 +1,11 @@
+resource "azurerm_log_analytics_workspace" "jeeb-uk" {
+  name                = "${var.short_prefix}-workspace"
+  location            = azurerm_resource_group.jeeb-uk.location
+  resource_group_name = azurerm_resource_group.jeeb-uk.name
+  sku                 = "Free"
+  retention_in_days   = 365
+}
+
 resource "azurerm_application_insights" "jeeb-uk" {
   name                = "${var.short_prefix}-appinsights"
   location            = azurerm_resource_group.jeeb-uk.location
