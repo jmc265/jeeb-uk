e23fcdf2a0dca777efc11353a477f7b6de88b4da
James Cross <me@james.cx>
2023-04-22 20:19:08 +0100

    Remove KV policy

--- .cloud/keyvault.tf
+++ .cloud/keyvault.tf
@@ -10,25 +10,25 @@ resource "azurerm_key_vault" "key-vault" {
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "terraform" {
-  key_vault_id = azurerm_key_vault.key-vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
+# resource "azurerm_key_vault_access_policy" "terraform" {
+#   key_vault_id = azurerm_key_vault.key-vault.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = data.azurerm_client_config.current.object_id
 
-  certificate_permissions = [
-    "Get",
-    "Import",
-    "Update",
-    "Delete",
-    "Purge",
-    "Recover"
-  ]
+#   certificate_permissions = [
+#     "Get",
+#     "Import",
+#     "Update",
+#     "Delete",
+#     "Purge",
+#     "Recover"
+#   ]
 
-  key_permissions = [
-    "Get"
-  ]
+#   key_permissions = [
+#     "Get"
+#   ]
 
-  secret_permissions  = [
-    "Get"
-  ]
-}
+#   secret_permissions  = [
+#     "Get"
+#   ]
+# }
