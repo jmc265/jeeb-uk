18a9cddd7cd5c53b52f0d2e950a29fbb1c95bf12
James Cross <me@james.cx>
2024-04-21 21:33:52 +0100

    New design

--- .eleventy.js
+++ .eleventy.js
@@ -3,18 +3,16 @@ const fs = require("fs");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const embedYouTube = require("eleventy-plugin-youtube-embed");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
-const markdownItGithubHeadings = require("markdown-it-github-headings")
 const slugify = require("./src/eleventy/slugify");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy({
-        "src/content/DigitalGarden/assets": "assets",
+        "src/content/posts/assets": "assets/",
+        "src/_includes/img": "assets/img",
         "src/_includes/css": "assets/css",
         "src/_includes/js": "assets/js",
-        "src/_includes/img": "assets/img",
         "src/_includes/favicon": "/"
     });
 
@@ -30,35 +28,43 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(embedYouTube, {
         lite: true
     });
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-    // Alias `layout: post` to `layout: layouts/post.njk`
     eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
-        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
-    });
     eleventyConfig.addFilter("limit", (arr, limit) => arr.slice(0, limit));
 
     // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
+    });
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
         return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
     });
 
-    // Override "posts" collection to not show drafts
+    // Override "posts" collection to sort by date
     eleventyConfig.addCollection("posts", (collection) => {
         return collection
-            .getFilteredByGlob("src/content/DigitalGarden/Posts/*.md")
-            .filter((post) => post.date <= new Date() && !post.data.draft)
+            .getFilteredByGlob("src/content/posts/*.md")
             .sort((p1, p2) => p1.date - p2.date);
     });
 
-    // Create Digital Garden collection
-    eleventyConfig.addCollection("digitalGarden", (collection) => {
-        return collection
-            .getFilteredByGlob("src/content/**/*.md")
-            .filter((post) => post.data.draft != true);
+    eleventyConfig.addCollection("postTags", function (collectionAPI) {
+        let collection = collectionAPI.getFilteredByGlob("src/content/posts/*.md");
+        let resultArrays = {};
+        collection.forEach((item) => {
+            item.data.tags.forEach((tag) => {
+                if (!resultArrays[tag]) { 
+                    resultArrays[tag] = []; 
+                }
+                resultArrays[tag].push(item);
+            });
+        });
+        return Object.entries(resultArrays).map((r) => ({tag: r[0], count: r[1].length})).sort((a, b) => b.count - a.count);
     });
+
+    eleventyConfig.setFrontMatterParsingOptions({
+		excerpt: true,
+	});
 
     // Customize Markdown library and settings:
     let markdownLibrary = markdownIt({
@@ -92,12 +98,18 @@ module.exports = function (eleventyConfig) {
             }
         }
     })
-        .use(require('markdown-it-modify-token'))
-        .use(markdownItGithubHeadings, {
-            className: "heading-anchor apply-svg-filter",
-            prefixHeadingIds: false
-        });
+    .use(require('markdown-it-modify-token'));
     eleventyConfig.setLibrary("md", markdownLibrary);
+
+    eleventyConfig.addFilter("markdownToHtml", str => {
+        let markdownLibrary = markdownIt({
+            html: true,
+            breaks: false,
+        });
+        markdownLibrary.linkify.set({});
+        markdownLibrary.disable('link');
+        return markdownLibrary.renderInline(str).replace(/\[([^\]]+)\]\([^)]+\)/g, '$1');
+    });
 
     return {
         templateFormats: [
