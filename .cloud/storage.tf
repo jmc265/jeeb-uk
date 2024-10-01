58e5a349c98caeb2572f6b9faa9a1675cbcdddc2
James Cross <me@james.cx>
2024-10-01 20:22:55 +0100

    Changes

--- .cloud/storage.tf
+++ .cloud/storage.tf
@@ -1,10 +1,10 @@
 resource "azurerm_storage_account" "jeeb-uk" {
-  name                      = "${var.short_prefix}storage"
-  resource_group_name       = azurerm_resource_group.jeeb-uk.name
-  location                  = azurerm_resource_group.jeeb-uk.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  enable_https_traffic_only = true
+  name                       = "${var.short_prefix}storage"
+  resource_group_name        = azurerm_resource_group.jeeb-uk.name
+  location                   = azurerm_resource_group.jeeb-uk.location
+  account_tier               = "Standard"
+  account_replication_type   = "LRS"
+  https_traffic_only_enabled = true
   static_website {
     index_document     = "index.html"
     error_404_document = "index.html"
